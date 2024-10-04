985f564dfbb55177621915e11847833609dd0cbc
Matt Pawlaczyk <168810098+automat9@users.noreply.github.com>
2024-10-04 14:56:44 +0100

    Create lab2.r

---
+++ Semester 1/Statistics and Mathematics for Business Analytics/labs/lab2.r
@@ -0,0 +1,40 @@
+## Q1: Correct way to assign 10 to a variable
+my_var <-10
+my_vector <- c(2,5,8,10,15)
+## Q2: Valid object type
+#a string
+#b vector
+#c dataframe
+#######d all of the above
+
+## Q3: Function used to create a sequence of numbers from 1 to 10
+seq(1,10)
+
+## Q4: How to calculate mean of a vector
+mean('vector')
+
+## Q5: How to create a logical vector in R that checks if each element in a numeric vector my_vector is greater than 10
+my_vector > 10
+
+## Q6: The poisson distribution is used to model:
+# # Prob of a specific number of events occuring in a fixed interval of time or space
+
+## Q7: Binomial dist is used to model: 
+# Prob of a specific number of successes in a fixed number of independent trials
+
+## Q8: Uniform distribution is used to model:
+# Events where all outcomes are equally likely
+
+## Q9: Normal dist is characterised by:
+# A bell shaped curve with most values clustered around the average
+
+## Q10: Exponential distribution is used to model:
+# Prob of an event occuring after a certain amount of time
+
+## Q11: 5% click rate and 200 potential customers
+# Binomial B~(200,0.05)
+
+## Q12: batches of 12 muffins, from past experience 10% muffin have flaws
+# B~(12,0.12)
+
+# DGP - data generating process (crucial in statistics)
