18e9f4fe8cf9dbc10edfaedb5bccb40e52266b4b
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-10-18 14:46:57 +0100

    Update and rename Notatki.py to Notatki.r

--- Semester 1/Statistics and Mathematics for Business Analytics/Notatki.py
+++
@@ -1,5 +0,0 @@
-# Normal Distribution
-# If random selection (no bias) then n = 30 is enough to ALWAYS generate a normal distribution
-#========================================================================================================
-# Binomial Distribution
-# p is determined based on past experience
---
+++ Semester 1/Statistics and Mathematics for Business Analytics/Notatki.r
@@ -0,0 +1,7 @@
+# Normal Distribution
+ If random selection (no bias) then n = 30 is enough to ALWAYS generate a normal distribution
+# Binomial Distribution
+ p is determined based on past experience
+
+
+Test_structure <- In open ended question you will be asked what will happen if n changes from 200 to 300 or if p changes 
