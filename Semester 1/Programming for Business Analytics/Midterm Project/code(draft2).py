516e4620d9d2e8fb8a585ad5b374031fcef9888c
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-07 18:03:08 +0000

    Update code(draft2).py

--- Semester 1/Programming for Business Analytics/Midterm Project/code(draft2).py
+++ Semester 1/Programming for Business Analytics/Midterm Project/code(draft2).py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib as plt
 
 # Load data from GitHub URL and reorder columns
 url = "https://github.com/automat9/Business-Analytics/raw/aadac54eaf6f4ca76c4970a1d317ad355a7fe051/Semester%201/Programming%20for%20Business%20Analytics/Midterm%20Project/Coffee_company.csv"
