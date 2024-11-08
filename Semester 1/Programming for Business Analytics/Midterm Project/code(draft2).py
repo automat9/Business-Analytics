70314a4d4548c7a19241ee6dc36afa48df6c5f1f
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-08 17:21:39 +0000

    Update code(draft2).py

--- Semester 1/Programming for Business Analytics/Midterm Project/code(draft2).py
+++ Semester 1/Programming for Business Analytics/Midterm Project/code(draft2).py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib as plt
 
-# Load data from GitHub URL and reorder columns
+# Load data from GitHub URL
 url = "https://github.com/automat9/Business-Analytics/raw/aadac54eaf6f4ca76c4970a1d317ad355a7fe051/Semester%201/Programming%20for%20Business%20Analytics/Midterm%20Project/Coffee_company.csv"
 data = pd.read_csv(url)
 
-data
+# removes extra space in column names
+data.columns = data.columns.str.strip()
+
+data.head()
