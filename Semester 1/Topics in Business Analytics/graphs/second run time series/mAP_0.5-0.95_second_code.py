0d88d32dae17e05291ab9389d13a9cb339578827
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-10 19:38:42 +0000

    Update mAP_0.5-0.95_second_code.py

--- Semester 1/Topics in Business Analytics/graphs/second run time series/mAP_0.5-0.95_second_code.py
+++ Semester 1/Topics in Business Analytics/graphs/second run time series/mAP_0.5-0.95_second_code.py
@@ -3,28 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the metadata
-data = pd.read_csv("https://raw.githubusercontent.com/automat9/Business-Analytics/refs/heads/master/Semester%201/Topics%20in%20Business%20Analytics/runs/results/results_metadata.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/automat9/Business-Analytics/refs/heads/master/Semester%201/Topics%20in%20Business%20Analytics/runs/results/2results.csv")
 
 # Remove white space from column names
 data.columns = data.columns.str.strip()
 
-###### Graph ######
-plt.figure(figsize=(10, 6))
-
-# Define colors for each group
-colours = {"first": "blue","second": "orange","third": "green"}
-    
-for column in data.columns[1:]: # Skip firt "epoch" column
-    group = column.strip().split('/')[0]  # Extract groups
-    plt.plot(data["epoch"], data[column], label=column, color=colours[group], linewidth=2)
-
-    
 # Plot config
-plt.xlabel("Epoch")
-plt.ylabel("mAP Score")
-plt.title("mAP Scores over Epochs")
+plt.figure(figsize=(10, 5))
+plt.plot(data["metrics/mAP_0.5:0.95"], label="mAP_0.5:0.95")
+plt.xlabel("epoch")
+plt.ylabel("mAP")
 plt.grid(True, alpha=1)
+plt.title("mAP_0.5:0.95 for Second Run")
 plt.legend()
 
 # Show the plot
 plt.show()
+
