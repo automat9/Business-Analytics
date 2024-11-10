2da39eadffed48b527cae1f6da7cb69fe63fa8fa
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-10 19:11:04 +0000

    Update 1.meta_code.py

--- Semester 1/Topics in Business Analytics/graphs/1.meta_code.py
+++ Semester 1/Topics in Business Analytics/graphs/1.meta_code.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the metadata
-data = pd.read_csv("https://raw.githubusercontent.com/automat9/Business-Analytics/214361ecbd0b86e0b7e38cf8ef66fff217dd79a3/Semester%201/Topics%20in%20Business%20Analytics/results/results_metadata.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/automat9/Business-Analytics/refs/heads/master/Semester%201/Topics%20in%20Business%20Analytics/runs/results/results_metadata.csv")
 
 # Remove white space from column names
 data.columns = data.columns.str.strip()
