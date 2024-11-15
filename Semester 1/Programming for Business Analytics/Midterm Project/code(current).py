922fa61d0f9be98842838f4edeeff040a89e0e15
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-15 16:16:22 +0000

    Update code(current).py

--- Semester 1/Programming for Business Analytics/Midterm Project/code(current).py
+++ Semester 1/Programming for Business Analytics/Midterm Project/code(current).py
@@ -1,6 +1,6 @@
 # Importing libraries
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # Load data from GitHub URL and reorder columns
 url = "https://github.com/automat9/Business-Analytics/raw/aadac54eaf6f4ca76c4970a1d317ad355a7fe051/Semester%201/Programming%20for%20Business%20Analytics/Midterm%20Project/Coffee_company.csv"
@@ -29,6 +29,15 @@ quarterly_sales = data['Sales'].resample('Q').sum().reset_index()
 quarterly_sales.columns = ['Quarter', 'Total Sales']
 print(quarterly_sales)
 
+# Plot a bar graph
+plt.figure(figsize=(10, 6))
+plt.bar(quarterly_sales['Quarter'].dt.to_period('Q').astype(str), quarterly_sales['Total Sales'], color='skyblue')
+plt.title('Quarterly Sales')
+plt.xlabel('Quarter')
+plt.ylabel('Total Sales')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 
-data.head()
+#data.head()
