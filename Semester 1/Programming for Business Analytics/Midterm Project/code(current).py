8a930e95e6afb1b0e5bdfd5443d0710639e9fa96
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-11-15 15:13:32 +0000

    Update and rename code(draft2).py to code(current).py

---
+++ Semester 1/Programming for Business Analytics/Midterm Project/code(current).py
@@ -0,0 +1,34 @@
+# Importing libraries
+import pandas as pd
+import matplotlib as plt
+
+# Load data from GitHub URL and reorder columns
+url = "https://github.com/automat9/Business-Analytics/raw/aadac54eaf6f4ca76c4970a1d317ad355a7fe051/Semester%201/Programming%20for%20Business%20Analytics/Midterm%20Project/Coffee_company.csv"
+data = pd.read_csv(url)
+
+# Remove white space
+data.columns = data.columns.str.strip()
+
+# Rename column
+data.rename(columns={'Month Name':'Month'}, inplace = True)
+
+# Floats into whole numbers
+data['Units Sold'] = data['Units Sold'].astype(int)
+
+# Total sales
+data['Sales'] = pd.to_numeric(data['Sales'].str.replace(',','', regex=False).str.replace('$','', regex=False), errors='coerce')
+data['Sales'] = data['Sales'].astype(float)
+total_sales = data['Sales'].sum()
+print('Total Sales:' '$', total_sales)
+
+# Quarterly sales
+data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y')
+data.set_index('Date', inplace=True)
+# Calculate
+quarterly_sales = data['Sales'].resample('Q').sum().reset_index()
+quarterly_sales.columns = ['Quarter', 'Total Sales']
+print(quarterly_sales)
+
+
+
+data.head()
