d0d79fc8b03dd88f2f8e98335d486e8890c92160
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-12-04 10:36:21 +0000

    Create pandas.py

---
+++ Semester 1/Programming for Business Analytics/Mock Exam/pandas.py
@@ -0,0 +1,17 @@
+# mean
+import pandas as pd
+data = {
+    'Name': ['Alice','Bob','Carol'],
+    'Age': [25,22,23],
+    'Score': [85,90,88]}
+df = pd.DataFrame(data)
+df['Score'].mean()
+
+# filter
+data = {
+    'Product': ['Apple','Banana','Cherry'],
+    'Price': [1.5,0.5,2.0],
+    'Quantity': [10,20,15]}
+df = pd.DataFrame(data)
+filtered = df[df['Price']>1] 
+print(filtered) # only those where price is > 1
