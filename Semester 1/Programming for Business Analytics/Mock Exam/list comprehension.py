bc1e0cf92cab298d21685491eb2889b8c90e25f4
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-12-04 10:16:11 +0000

    Update list comprehension.py

--- Semester 1/Programming for Business Analytics/Mock Exam/list comprehension.py
+++ Semester 1/Programming for Business Analytics/Mock Exam/list comprehension.py
@@ -1,7 +1,19 @@
+#1
 numbers = [1,2,3,4,5]
 new_numbers = [num ** 2 for num in numbers]
 print(new_numbers) # changes to squared
 
+#2
 numbers = [10,15,20,25,30]
 even = [num for num in numbers if num % 2 == 0]
 print(even) # even only
+
+#3
+words = ['apple', ' banana', 'cherry', 'date']
+length = [len(word) for word in words]
+print(length)
+
+#4
+numbers = [5,10,15,20,25]
+multiplied = [num * index for index, num in enumerate(numbers)]
+print(multiplied) # multiplied elements by their respective indices
