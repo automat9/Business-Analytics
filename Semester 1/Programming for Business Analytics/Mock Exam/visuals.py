9aa14cde5280d4e7c970a713e4848a1223274ab5
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-12-04 09:54:48 +0000

    Create visuals.py

---
+++ Semester 1/Programming for Business Analytics/Mock Exam/visuals.py
@@ -0,0 +1,22 @@
+# 1
+import matplotlib.pyplot as plt
+
+products = ['A', 'B', 'C', 'D', 'E']
+sales = [50, 70, 30, 85, 60]
+
+plt.bar(products, sales)
+plt.title('Product Sales Comparison')
+plt.xlabel('Products')
+plt.ylabel('Sales')
+plt.show()
+
+# 2
+day = [1,2,3,4,5]
+visitors = [100,150,200,250,300]
+
+plt.plot(day, visitors)
+plt.ylabel('Day')
+plt.xlabel('Visitors')
+plt.title('Number of visitors per day')
+plt. grid()
+plt.show()
