e36d4c7d2cca49a3da01b31ef953d532e3cb1804
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-12-06 15:43:33 +0000

    Update string manipulation.py

--- Semester 1/Programming for Business Analytics/Mock Exam/string manipulation.py
+++ Semester 1/Programming for Business Analytics/Mock Exam/string manipulation.py
@@ -1,6 +1,10 @@
-# Backwards
-text = 'Hello'
-print(text[::-1])
+# [start:end:step]
+string = 'This is a short sentence'
+string[:4] # This
+string[4:] #  is a short sentence
+string[::2] # Ti sasotsnec
+string[::-1] # ecnetnes trohs a si sihT
+string[3::-1] # sihT
 
 # Count vowels
 text2 = "programming is fun"
@@ -14,3 +18,11 @@ print(count)
 text3 = "I love Python because Python is powerful."
 replaced = text3.replace('Python', 'Javascript')
 print(replaced)
+
+# Find coordinates of word, if multiple instances use re
+string = "Hello, World!"
+word = 'World'
+start_index = string.find(word)
+end_index = start_index + len(word)
+result = string[start_index:end_index]
+print('The coordinates of your word are:', start_index, end_index)
