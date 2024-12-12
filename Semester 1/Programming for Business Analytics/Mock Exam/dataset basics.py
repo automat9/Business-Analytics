9c3c6a2e42e54424d38518174616927392e53f45
Matt Paw <168810098+automat9@users.noreply.github.com>
2024-12-12 09:10:06 +0000

    Create dataset basics.py

---
+++ Semester 1/Programming for Business Analytics/Mock Exam/dataset basics.py
@@ -0,0 +1,25 @@
+import pandas as pd
+df = pd.read_csv("C:\\Users\\mp967\\Desktop\\Coffee_company.csv")
+# or
+df = pd.read_csv(r"C:\Users\mp967\Desktop\Coffee_company.csv")
+
+# remove white space from columns
+df.columns = df.columns.str.strip()
+
+# check all columns for missing
+missing_per_column = df.isnull().sum()
+
+# remove rows with missing
+df = df.dropna(axis=0)
+# remove columns with missing
+df = df.dropna(axis=1)
+
+# removing white space, $, and ,
+df['Gross Sales'] = (
+    df['Gross Sales']
+    .str.strip()
+    .str.replace('$', '', regex=False)
+    .str.replace(',','')
+    .astype(float))
+
+avg = sum(df['Gross Sales']) / len(df['Gross Sales'])
