f4a52e5ea2c0779bab8d3786395a2c953b1482f8
Matt Paw <168810098+automat9@users.noreply.github.com>
2025-02-17 13:19:40 +0000

    Create 7.new car data (replace old car data from files 5 and 6).py

---
+++ Semester 2/Machine Learning for Social Data Science/Assignments/7.new car data (replace old car data from files 5 and 6).py
@@ -0,0 +1,10 @@
+new_car_data = {
+    'year': [2005],
+    'model': ['Explorer'],       
+    'trim': ['XLT'],           
+    'body': ['SUV'],          
+    'transmission': ['automatic'],
+    'condition': [45],
+    'odometer': [130000],
+    'color': ['black'],           
+    'interior': ['gray']}
